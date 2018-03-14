a7705828464992e1e63df062aafc7539301ef622
Dave Richardson <dave@zygomorph.net>
2018-03-14 15:48:43 +0000

    added linting support with ESLint

--- build_scripts/srcServer.js
+++ build_scripts/srcServer.js
@@ -5,6 +5,8 @@ import open from 'open';
 import webpack from 'webpack';
 import config from '../webpack.config.dev';
 
+/* eslint-disable no-console */
+
 const port = 3000;
 const app = express();
 
