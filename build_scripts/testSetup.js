d58f993840eed02eb242bf508bd6aae568d84b70
Dave Richardson <dave@zygomorph.net>
2018-03-15 14:02:49 +0000

    added second unit test with jsdom

--- build_scripts/testSetup.js
+++ build_scripts/testSetup.js
@@ -1,7 +1,7 @@
-// This file isn't transpiled, so must use CommonJS and ES5
+// this file isn't transpiled, so must use CommonJS and ES5
 
-// Register Babel to transpile before our tests run
+// register Babel to transpile before our tests run
 require('babel-register')();
 
-// Disable webpack features that Mocha doesn't understand
+// disable webpack features that Mocha doesn't understand
 require.extensions['.css'] = function() {};
