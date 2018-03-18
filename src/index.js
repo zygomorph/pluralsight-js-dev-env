807cecd0749b9ff86d3505f3a94e29a41ef61fd2
Dave Richardson <dave@zygomorph.net>
2018-03-18 23:33:33 +0000

    centralising HTTP requests and adding a simple Api

--- src/index.js
+++ src/index.js
@@ -1,7 +1,42 @@
+import {getUsers} from './api/userApi';
+
 import './index.css';
 
-import numeral from 'numeral';
+//import numeral from 'numeral';
 
-const courseValue = numeral(1000).format('$0,0.00');
+//const courseValue = numeral(1000).format('$0,0.00');
 //debugger;
-console.log(`I would pay ${courseValue} for this awesome course!`);  // eslint-disable-line no-console
+//console.log(`I would pay ${courseValue} for this awesome course!`);  // eslint-disable-line no-console
+
+// Populate table of users via API call.
+getUsers().then(result => {
+  let usersBody = "";
+
+  result.forEach(user => {
+    usersBody+= `<tr>
+      <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
+      <td>${user.id}</td>
+      <td>${user.firstName}</td>
+      <td>${user.lastName}</td>
+      <td>${user.email}</td>
+      </tr>`
+  });
+
+  global.document.getElementById('users').innerHTML = usersBody;
+
+  //const deleteLinks = global.document.getElementsByClassName('deleteUser');
+
+  // Must use array.from to create a real array from a DOM collection
+  // getElementsByClassname only returns an "array like" object
+  /*
+  Array.from(deleteLinks, link => {
+    link.onclick = function(event) {
+      const element = event.target;
+      event.preventDefault();
+      deleteUser(element.attributes["data-id"].value);
+      const row = element.parentNode.parentNode;
+      row.parentNode.removeChild(row);
+    };
+  });
+  */
+});
